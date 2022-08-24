825a1bcf64a3f7a9bd936f95cd69ab84111e20b6
Grim1312 <111816007+Grim1312@users.noreply.github.com>
2022-08-25 00:55:21 +0200

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Binaries\Release" -ArchiveFileName "birdbrainmofo.zip" -Format Zip
+powershell Compress-7Zip "Update\Binaries\Release" -ArchiveFileName "birdbrainmofo.zip" -Format Zip
 :exit
 popd
 @echo on
